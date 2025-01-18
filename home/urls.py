23c56263f3fb740b935431a99b450d889fedab63
Rahul <docostar@panel.rokan.online>
2025-01-18 16:13:13 +0530

    Related Product images and detail done

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view,shop_view,product_detail
+from .views import home_view,shop_view,product_detail,demo_product_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('shop',shop_view , name='shop'),
-    path('product/<int:product_id>/', product_detail, name='product_detail')
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('demo', demo_product_view, name='demo'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,7 +8,11 @@ def home_view(request):
 def shop_view(request):
     return render(request, 'home/base.html')
 
+def demo_product_view(request):
+    return render(request, 'home/product_detail.html')
+
 def product_detail(request,product_id):
     product = get_object_or_404(Product, id=product_id)
+    related_products = Product.objects.filter(category=product.category).exclude(id=product.id)[:4] 
     benefits_list = product.benefits.split(",") if product.benefits else []
-    return render(request, 'home/products.html', {'product': product, 'benefits_list': benefits_list})
+    return render(request, 'home/products.html', {'product': product, 'benefits_list': benefits_list, 'related_products':related_products})
