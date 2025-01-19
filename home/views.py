4ebf29ae701500ccdbf1071db8fcfd92a94033fe
Rahul <docostar@panel.rokan.online>
2025-01-19 09:40:42 +0530

    mystyle.css added and set related product size

--- home/views.py
+++ home/views.py
@@ -13,6 +13,6 @@ def demo_product_view(request):
 
 def product_detail(request,product_id):
     product = get_object_or_404(Product, id=product_id)
-    related_products = Product.objects.filter(category=product.category).exclude(id=product.id)[:4] 
+    related_products = Product.objects.filter(category=product.category).exclude(id=product.id)[:4]
     benefits_list = product.benefits.split(",") if product.benefits else []
     return render(request, 'home/products.html', {'product': product, 'benefits_list': benefits_list, 'related_products':related_products})
